58e80825b11ae991d9d086f38f5245a0091536a6
Rudreshwar Baranwal <baranwalrudreshwar@gmail.com>
2026-07-28 11:36:27 +0530

    updated navbar styles

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+import { CurrentUserService } from '../../../core/services/current-user.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar {
+  private readonly currentUserService = inject(CurrentUserService);
+
+  readonly currentUser$ = this.currentUserService.currentUser$;
+
+  showProfile = false;
+
+  toggleProfile(): void {
+    this.showProfile = !this.showProfile;
+  }
+}
